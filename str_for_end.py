650d7b8c1b7017dbfaa60ea71dd37a872f05381d
sathwik <svemulap@student.gitam.edu>
2026-02-04 11:41:08 +0530

    Create str_for_end.py

---
+++ str_for_end.py
@@ -0,0 +1,15 @@
+s1 = "Hello"
+s2 = 'gitam'
+
+for i in range (1,9):
+ print(s1+s2,end=" ")
+ print(s1)
+ '''Hellogitam Hello
+Hellogitam Hello
+Hellogitam Hello
+Hellogitam Hello
+Hellogitam Hello
+Hellogitam Hello
+Hellogitam Hello
+Hellogitam Hello
+ '''
